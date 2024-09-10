f188f59782e459122a3c8b20db9fa2d9d468b110
23DP1DOrlo <166371590+23DP1DOrlo@users.noreply.github.com>
2024-09-10 10:31:39 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,12 +1,26 @@
 package lv.rvt;
 
+import java.util.Scanner;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-     System.out.println("Once upon a time");    
-     System.out.println("there was");
-     System.out.println("a program");
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in); // Izveidojam skaneri vienu reizi, un pielietojam visā programmā
+
+        System.out.println("Ievadi savu vārdu:");
+        String name = scanner.nextLine();  
+        System.out.println("Ievadiet savu uzvārdu:");
+        String lastname = scanner.nextLine();
+        System.out.println("Ievadiet savu grupu");
+        String group = scanner.nextLine();
+
+        // Izvade vienā rindiņā izmantojot 3 mainīgos
+        // Output example: Students: <name> <surname>, grupa: <group>
+
+        // String concatenation
+        System.out.println(name + " " + lastname + " " + "grupa: " + group);
+
+     
     }
 
 }
