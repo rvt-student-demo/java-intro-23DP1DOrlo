0d0ff82bd202c97cccd56d00a8f06470a479fae0
Dmitrijs Orlovics <A230966DO@rvt.lv>
2024-09-17 13:31:25 +0300

    Larger Than or Equal To

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,22 +5,19 @@ import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args ) {
-        Scanner scanner = new Scanner(System.in); // Izveidojam skaneri vienu reizi, un pielietojam visā programmā
+        Scanner scanner = new Scanner(System.in); // Izveidojam skaneri vienu reizi, un pielietojam visā programm
+           
+        System.out.println("Ievadiet 1. skaitli: ");
+        int number1 = Integer.valueOf(scanner.nextLine());
+        System.out.println("Ievadiet 2. skaitli: ");
+        int number2 = Integer.valueOf(scanner.nextLine());
 
-        System.out.println("Ievadi savu vārdu:");
-        String name = scanner.nextLine();  
-        System.out.println("Ievadiet savu uzvārdu:");
-        String lastname = scanner.nextLine();
-        System.out.println("Ievadiet savu grupu");
-        String group = scanner.nextLine();
-
-        // Izvade vienā rindiņā izmantojot 3 mainīgos
-        // Output example: Students: <name> <surname>, grupa: <group>
-
-        // String concatenation
-        System.out.println(name + " " + lastname + " " + "grupa: " + group);
-
-     
+        if (number1 > number2) {
+            System.out.println("Lielākais skaitlis ir: " + number1);
+        } 
+        else if (number2 > number1) {
+            System.out.println("Lielākais skaitlis ir: " + number2);
+        }
     }
 
 }
