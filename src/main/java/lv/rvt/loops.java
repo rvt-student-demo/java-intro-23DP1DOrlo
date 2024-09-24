9b1786b64f2b1b2f5d4b4d94874c3929999833d8
Dmitrijs Orlovics <A230966DO@rvt.lv>
2024-09-24 13:53:33 +0300

    Sum of a sequence

--- src/main/java/lv/rvt/loops.java
+++ src/main/java/lv/rvt/loops.java
@@ -5,11 +5,13 @@ import java.util.Scanner;
 public class loops {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int summa = 0;
         System.out.println("Write a number: ");
         int input = Integer.valueOf(scanner.nextLine());
-        for(int i = input; i < 101; i++) {
-            System.out.println(i);
+        for(int i = 1; i < (input+1);i++) {
+            summa = summa + i;
         }
+        System.out.println(summa);
     }
 }
 
