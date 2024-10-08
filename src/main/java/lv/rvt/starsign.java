a33f61745f2ca65ada8a01f309d47a06fde37616
Dmitrijs Orlovics <A230966DO@rvt.lv>
2024-10-08 13:07:04 +0300

    Advanced astrology

--- src/main/java/lv/rvt/starsign.java
+++ src/main/java/lv/rvt/starsign.java
@@ -2,6 +2,7 @@ package lv.rvt;
 
 public class starsign {
     public static void main(String[] agrs){
+        // Star Sign
         printStars(5);
         printStars(3);
         printStars(9);
@@ -11,10 +12,17 @@ public class starsign {
         printRectangle(5, 2);
         System.out.println("");
         printTriangle(4);
+
+        // Advanced astrology
+        printSpaces(4);
+        System.out.println("");
+        printTriangle2(4);
+        System.out.println("");
+        christmasTree(5);
     }
 
     public static void printStars(int number){
-        for(int i = 1; i <= number;i++){
+        for(int i = 0; i < number;i++){
             System.out.print('*');
         }
         System.out.println("");
@@ -40,6 +48,29 @@ public class starsign {
             printStars(i);
                 }
     }
+    public static void printSpaces(int number_for_spaces) {
+        for(int i = 0; i < number_for_spaces;i++){
+            System.out.print(" ");
+        }
+        System.out.print("");
+    }
+    public static void printTriangle2(int triangeSize2){
+        for(int i = 0; i <= triangeSize2; i++){
+            printSpaces(triangeSize2 - i);
+            printStars(i);
+        }
+    }
+    public static void christmasTree(int heightTree) {
+        for(int i = 0; i <= heightTree; i++){
+            printSpaces(heightTree - i);
+            printStars(2*i-1);
+        }
+        for(int i = 1; i < 3; i++){
+            printSpaces(heightTree-2);
+            printStars(3);
+        }
+    }
+
     
 
 }
