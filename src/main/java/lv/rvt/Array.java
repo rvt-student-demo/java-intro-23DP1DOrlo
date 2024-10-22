1a2153143f708ba801d3c128f450447deb0b2894
Dmitrijs Orlovics <A230966DO@rvt.lv>
2024-10-22 09:35:56 +0300

    Only these numbers

---
+++ src/main/java/lv/rvt/Array.java
@@ -0,0 +1,15 @@
+package lv.rvt;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> wordList = new ArrayList<>();
+
+        while(true){
+            System.out.println();
+        }
+        
+    }
+}
---
+++ src/main/java/lv/rvt/pashol.java
@@ -0,0 +1,28 @@
+package lv.rvt;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class pashol {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
+        while(true){ 
+            int number = scanner.nextInt();
+            if(number == -1){
+                break;
+            }   
+            numbers.add(number);
+        }
+        System.out.print("From where? ");
+        int start = scanner.nextInt();
+        System.out.print("To where? ");
+        int fin = scanner.nextInt();
+        for(int i = start; i <= fin; i++){
+            System.out.println(numbers.get(i));
+        }
+        scanner.close();
+
+
+    }
+}
