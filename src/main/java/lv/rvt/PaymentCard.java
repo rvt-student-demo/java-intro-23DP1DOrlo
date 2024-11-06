735a33324d71998ef9ca52ef996babf7d52b14a6
Dmitrijs Orlovics <A230966DO@rvt.lv>
2024-11-06 17:09:17 +0200

    Payment Card

---
+++ src/main/java/lv/rvt/PaymentCard.java
@@ -0,0 +1,39 @@
+package lv.rvt;
+
+public class PaymentCard {
+        private double balance;
+        private double amount;
+    
+        public PaymentCard(double openingBalance) {
+            this.balance = openingBalance;
+        }
+        
+        @Override
+        public String toString() {
+            return "The card has a balance of "+ balance + " euros";
+        }
+
+        public void eatAffordably() {
+            if(balance - 2.60 >= 0) {
+                balance -= 2.60;
+            }
+        }
+        
+        public void eatHeartily() {
+            if(balance - 4.60 >= 0) {
+                balance -= 4.60;
+            }        
+        }
+
+        public void addMoney(double amount) {
+            if(amount < 0){
+                balance = balance;
+            }
+            else if(balance + amount > 150){
+                balance = 150;
+            }
+            else {
+                balance += amount;
+            }
+        }
+    }
